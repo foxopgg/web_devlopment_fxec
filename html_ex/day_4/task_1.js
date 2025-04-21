cf295300d73ae60259b050e94d0318ca7f0b71be
Anto Bivin <antobivin@gmail.com>
2025-04-21 14:30:00 +0530

    added ex ample day 4

---
+++ html_ex/day_4/task_1.js
@@ -0,0 +1,15 @@
+// Declaring variables with different data types
+let stringVar = "Hello, World!";
+let numberVar = 42;
+let booleanVar = true;
+let undefinedVar;
+let nullVar = null;
+let objectVar = { name: "John", age: 30 };
+
+// Logging their types in the console
+console.log("Type of stringVar: ", typeof stringVar);  // string
+console.log("Type of numberVar: ", typeof numberVar);  // number
+console.log("Type of booleanVar: ", typeof booleanVar);  // boolean
+console.log("Type of undefinedVar: ", typeof undefinedVar);  // undefined
+console.log("Type of nullVar: ", typeof nullVar);  // object (a quirk in JavaScript)
+console.log("Type of objectVar: ", typeof objectVar);  // object
